f03bf46374df62a33fbe905afad1c7e7d73df5cc
Luke <lukeskywalker1105@msn.com>
2016-05-19 15:34:11 -0400

    Initial Commit.

---
+++ Instant Gist/GistUploader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Instant_Gist
+{
+    class GistUploader
+    {
+        private bool LoggedIn { get; }
+        private string UserName { get; }
+        private 
+    
+    }
+}
---
+++ Instant Gist/GithubLogin.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Instant_Gist
+{
+    /// <summary>
+    /// Interaction logic for GithubLogin.xaml.
+    /// </summary>
+    [ProvideToolboxControl("Instant_Gist.GithubLogin", true)]
+    public partial class GithubLogin : UserControl
+    {
+        public GithubLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format(CultureInfo.CurrentUICulture, "We are inside {0}.Button1_Click()", this.ToString()));
+        }
+    }
+}
