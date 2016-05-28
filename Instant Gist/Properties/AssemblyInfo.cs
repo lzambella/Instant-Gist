854623e081ae02729c3db7ab106c16a8c53d43c3
Luke <lukeskywalker1105@msn.com>
2016-05-28 14:02:36 -0400

    Add assembly info.

--- Instant Gist/Properties/AssemblyInfo.cs
+++ Instant Gist/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Instant_Gist")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyTitle("InstantGist")]
+[assembly: AssemblyDescription("Quickly upload code snippets.")]
+[assembly: AssemblyConfiguration("debug")]
+[assembly: AssemblyCompany("Luke Zambella")]
 [assembly: AssemblyProduct("Instant_Gist")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,3 +32,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
